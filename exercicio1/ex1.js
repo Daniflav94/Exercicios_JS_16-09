e0cb75f7dfc4f206275a2fcf00e06f1a292c78ad
Daniele Almeida <99519903+Daniflav94@users.noreply.github.com>
2022-09-17 00:50:47 -0300

    Update ex1.js

--- exercicio1/ex1.js
+++ exercicio1/ex1.js
@@ -1,8 +1,13 @@
 //1. Dado o tamanho da base e da altura de um retângulo, calcular a sua área e o seu perímetro.
-
-const base = Number(prompt("Digite o valor da base do retângulo: "))
-const altura = Number(prompt("Digite o valor da altura do retângulo: "))
+ 
+let base = Number(prompt("Digite o valor da base do retângulo: "))
+let altura = Number(prompt("Digite o valor da altura do retângulo: "))
 
 const area = base * altura
+const perimetro = base * 2 + altura * 2
+
 
 alert("Área do retângulo: " + area)
+
+
+alert("Perímetro do retângulo: " + perimetro )
